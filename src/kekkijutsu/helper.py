5af674bb71e9c9471f379e39af09b7e4bafae9c8
hxari <hxari@proton.me>
2025-07-15 13:46:12 +0700

    [CREATE] Created src/kekkijutsu/helper.py

---
+++ src/kekkijutsu/helper.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+#
+# @author hxAri (hxari)
+# @create 15-07-2025 12:39
+# @github https://github.com/hxAri/Kekkijutsu
+#
+# Kekkijutsu is a powerful Python Project builder.
+#
+# Kekkijutsu Copyright (c) 2025 - hxAri <hxari@proton.me>
+# Kekkijutsu Licence under GNU General Public Licence v3
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+#
+# This program was built and created under Kekkijutsu.
+# Visit the github page <https://github.com/hxAri/Kekkijutsu>.
+#
+
+from click.core import Group
+from typing import final
+
+
+__all__ = [
+	"Helper"
+]
+
+
+@final
+class Helper( Group ):
+	
+	""" Kekkijutsu Custom Help Command """
+	
+	...
